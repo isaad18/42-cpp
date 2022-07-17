1a6f9239486be9d317b77bfd6688629f7f68e6ae
isaad18 <em4d2002@gmail.com>
2022-07-17 14:49:22 +0400

    CPP

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -29,14 +29,3 @@ void Fixed::setRawBits(int const raw){
 	std::cout << "setRawBits member function called\n";
 	this->value = raw;
 }
-
-int main( void ) {
-Fixed a;
-Fixed b( a );
-Fixed c;
-c = b;
-std::cout << a.getRawBits() << std::endl;
-std::cout << b.getRawBits() << std::endl;
-std::cout << c.getRawBits() << std::endl;
-return 0;
-}
