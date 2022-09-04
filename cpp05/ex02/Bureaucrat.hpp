cec3aae64314f8f3f3d33c136a3d0ec5e799a49d
Imad Saad <isaad@lab3r4s7.42abudhabi.ae>
2022-09-04 13:47:57 +0400

    CPP

---
+++ cpp05/ex02/Bureaucrat.hpp
@@ -0,0 +1,37 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include <exception>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat {
+	private:
+		const std::string name;
+		int grade;
+	public:
+		Bureaucrat(const std::string &name, int grade);
+		Bureaucrat();
+		virtual ~Bureaucrat();
+		std::string getName() const;
+		int getGrade() const;
+		void incGrade();
+		void decGrade();
+		Bureaucrat &operator=(const Bureaucrat &other);
+		class GradeTooHighException: public std::exception
+		{
+			const char* what() const throw();
+		};
+		class GradeTooLowException: public std::exception
+		{
+			const char* what() const throw();
+		};
+		void signForm(Form &F);
+};
+std::ostream &operator<<(std::ostream &s, Bureaucrat const &_new);
+
+
+#endif
---
+++ cpp05/ex02/main.cpp
@@ -0,0 +1,38 @@
+#include "Bureaucrat.hpp"
+
+void	init_form(std::string name, int sGrade, int eGrade)
+{
+	try
+	{
+		Form form = Form(name, sGrade, eGrade);
+		std::cout << form << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+
+int	main(void)
+{
+	init_form("Form1", 1, 150);
+	init_form("Form4", 1, 151);
+	init_form("Form3", -1, 150);
+	init_form("Form5", -2, 155);
+
+	Bureaucrat	ber1 = Bureaucrat("employee", 100);
+	Bureaucrat	ber2 = Bureaucrat("President", 1);
+	Form	form1 = Form("low", 144, 120);
+	Form	form2 = Form("medium", 99, 80);
+	Form	form3 = Form("topsecret", 3, 2);
+
+	std::cout << std::endl << "Employee" << std::endl;
+	ber1.signForm(form1);
+	ber1.signForm(form2);
+	ber1.signForm(form3);
+	std::cout << "President" << std::endl;
+	ber2.signForm(form1);
+	ber2.signForm(form2);
+	ber2.signForm(form3);
+}
